faa85cb5b632fb8006eb8973c91c42d9f998c96f
Iwan Nijs - de Boer <iwan@nicethoughts.eu>
2023-05-03 15:54:58 +0200

    Made button code reliable by introducing debounce logic.

--- picoed_v2_lib/button.py
+++ picoed_v2_lib/button.py
@@ -1,33 +1,40 @@
-from machine import Pin
-from time import sleep_ms
+from machine import Pin, Timer
 
 
 class Button:
-    def __init__(self, gpio_nr):
-        self._button = Pin(gpio_nr, Pin.IN, Pin.PULL_UP)
+    def __init__(self, gpio_nr, pressed_value=False):
+        self._pin = Pin(gpio_nr, Pin.IN, Pin.PULL_UP)
+        self._pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self._irq_handler)
+        self._pressed_value = pressed_value
+        self._DEBOUNCE_MS = 20
         self._presses = 0
-        self._flags = None
-        self._is_pressed = False
-        self._button.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self.callback)
+        self.db_timer = Timer(-1)
 
-    def callback(self, pin: Pin):
-        flags = pin.irq().flags()
-        self._flags = flags
-        if flags & Pin.IRQ_FALLING:
-            self._is_pressed = True
+    def _irq_handler(self, pin: Pin):
+        self.db_timer.init(mode=Timer.ONE_SHOT, period=self._DEBOUNCE_MS, callback=self._debounce_handler)
+        self._pin.irq(trigger=0)
+
+    def _debounce_handler(self, t: Timer):
+        if self._pin.value() == self._pressed_value:
             self._presses += 1
-        else:
-            self._is_pressed = False
+        self._pin.irq(trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING, handler=self._irq_handler)
+
+    @property
+    def presses(self):
+        return self._presses
 
     def is_pressed(self):
-        return self._is_pressed
+        return self._pin.value() == self._pressed_value
 
-    def presses(self):
+    def reset_presses(self):
         presses = self._presses
         self._presses = 0
         return presses
 
     def was_pressed(self):
-        presses = self._presses
-        self._presses = 0
-        return presses > 0
+        if self._presses == 0:
+            return 0
+        else:
+            presses = self._presses
+            self._presses = 0
+            return presses
