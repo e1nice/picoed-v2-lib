6363a53e78e59e42eb6e730a5216eb58a3889ad0
Iwan Nijs - de Boer <iwan@nicethoughts.eu>
2023-05-06 11:01:42 +0200

    Changed pins used for the ultrasonic sensor example.

--- examples/example_ultrasonic.py
+++ examples/example_ultrasonic.py
@@ -3,7 +3,7 @@ from picoed_v2_lib.ultrasonic import Ultrasonic
 from machine import Pin
 from time import sleep_ms
 
-ultra = Ultrasonic(Pin(Gpio.P1, Pin.OUT), Pin(Gpio.P2, Pin.IN))
+ultra = Ultrasonic(Pin(Gpio.P13, Pin.OUT), Pin(Gpio.P14, Pin.IN))
 
 for x in range(20):
     sleep_ms(200)
